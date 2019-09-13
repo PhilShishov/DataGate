6432f12f43129f72e88796d9931f0723f43e75ba
Philip Shishov <philshishov@gmail.com>
2019-09-13 10:26:43 +0200

    Created basic design for index and login page

---
+++ src/PharusDataGateApp/Pharus.App/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharus.App.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
