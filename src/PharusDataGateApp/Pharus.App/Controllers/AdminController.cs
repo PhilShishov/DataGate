5e486131c58d7df18a3c7abd288ef5ce0fc449aa
Philip Shishov <philshishov@gmail.com>
2019-09-16 17:57:38 +0200

    Created policy authorization for admin

---
+++ src/PharusDataGateApp/Pharus.App/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿namespace Pharus.App.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    [Authorize(Policy = "RequireAdminRole")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
