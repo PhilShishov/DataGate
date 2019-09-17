0c78f5314b6db751020a6a53d439e991ca6d9acc
Philip Shishov <philshishov@gmail.com>
2019-09-17 16:58:31 +0200

    Added ForgotPassword form with confirmation

---
+++ src/PharusDataGateApp/Pharus.App/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pharus.App.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+    }
+}
