11eb2707ebcc67fc3c61f4dac9aa964dd7dd38bc
Philip Shishov <philshishov@gmail.com>
2019-10-09 11:14:01 +0200

    Added error view for exception throwing

---
+++ src/PharusDataGateApp/Pharus.App/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pharus.App.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
