f3e56905a6614a3913767add55c406ac96e91d2d
Philip Shishov <philshishov@gmail.com>
2019-10-14 10:34:59 +0200

    Refactored services with less repeated code

---
+++ src/PharusDataGateApp/Pharus.Services/ShareClassesService.cs
@@ -0,0 +1,31 @@
+﻿namespace Pharus.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Pharus.Domain.Pharus_vFinale;
+    using Pharus.Services.Contracts;
+
+    public class ShareClassesService : IShareClassesService
+    {
+        public List<string[]> GetAllActiveShareClasses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> GetAllActiveShareClasses(DateTime? chosenDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TbHistoryShareClass> GetAllShareClasses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TbHistoryShareClass GetShareClass(string fundName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PharusDataGateApp/Pharus.Services/SubFundsService.cs
@@ -0,0 +1,45 @@
+﻿namespace Pharus.Services
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Pharus.Data;
+    using Pharus.Services.Contracts;
+    using Pharus.Domain.Pharus_vFinale;
+
+    public class SubFundsService : ISubFundsService
+    {
+        private readonly Pharus_vFinaleContext context;
+
+        public SubFundsService(
+            Pharus_vFinaleContext context)
+        {
+            this.context = context;
+        }
+
+        public List<string[]> GetAllActiveSubFunds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string[]> GetAllActiveSubFunds(DateTime? chosenDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TbHistorySubFund> GetAllSubFunds()
+        {
+            var subFunds = this.context.TbHistorySubFund.ToList();
+
+            return subFunds;
+        }
+
+        public TbHistorySubFund GetSubFund(string subFundName)
+        {
+            var subFund = this.context.TbHistorySubFund.FirstOrDefault(f => f.SfOfficialSubFundName == subFundName);
+
+            return subFund;
+        }
+    }
+}
