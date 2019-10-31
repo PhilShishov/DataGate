5c799c81b38a9b115073f7bffc014743eeed2b41
Philip Shishov <philshishov@gmail.com>
2019-10-31 10:22:02 +0100

    Enums for ShareClasses

---
+++ src/PharusDataGateApp/Pharus.Domain/Enums/ShareClasses/TbDomShareStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Pharus.Domain.Enums.ShareClasses
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TbDomShareStatus
+    {
+        [Display(Name = "Active")]
+        Active = 1,
+
+        [Display(Name = "Inactive - Liquidated")]
+        InactiveLiquidated = 2,
+
+        [Display(Name = "Inactive - Closed")]
+        InactiveClosed = 3,
+
+        [Display(Name = "Inactive - To Be Launched")]
+        InactiveToBeLaunched = 4,
+
+        [Display(Name = "Inactive - Merged")]
+        InactiveMerged = 5
+    }
+}
