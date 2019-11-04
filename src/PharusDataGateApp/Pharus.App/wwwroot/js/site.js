f131a6bbeff7d48cbb23c818d6f7391c72674c99
Philip Shishov <philshishov@gmail.com>
2019-11-04 16:02:54 +0100

    New table hover design

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/site.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/site.js
@@ -13,14 +13,13 @@ function loadDate() {
 
         document.getElementById("fundDatetime").value = date;
     }
-}
+    function formatDate(date) {
+        var day = ('0' + date.getDate()).slice(-2);
+        var month = ('0' + (date.getMonth() + 1)).slice(-2);
+        var year = date.getFullYear();
 
-function formatDate(date) {
-    var day = ('0' + date.getDate()).slice(-2);
-    var month = ('0' + (date.getMonth() + 1)).slice(-2);
-    var year = date.getFullYear();
-
-    return year + '-' + month + '-' + day;
+        return year + '-' + month + '-' + day;
+    }
 }
 
 //TODO js function bug with bootstrap
