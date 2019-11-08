053084331efc26aec0899871ffa209d7c975b08f
Philip Shishov <philshishov@gmail.com>
2019-11-08 16:04:22 +0100

    Refactored all onclick, onfocus and onload events with window.onload and addeventlistener

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/getDerivatives.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/getDerivatives.js
@@ -20,18 +20,3 @@
         derivPurposeLbl.setAttribute('hidden', true);
     }
 }
-
-function loadDate() {
-    if (document.getElementById("fundDatetime").value == "") {
-        let date = (formatDate(new Date())).toString();
-
-        document.getElementById("fundDatetime").value = date;
-    }
-    function formatDate(date) {
-        var day = ('0' + date.getDate()).slice(-2);
-        var month = ('0' + (date.getMonth() + 1)).slice(-2);
-        var year = date.getFullYear();
-
-        return year + '-' + month + '-' + day;
-    }
-}
---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/loadDate.js
@@ -0,0 +1,14 @@
+﻿function loadDate() {
+    if (document.getElementById("fundDatetime").value == "") {
+        let date = (formatDate(new Date())).toString();
+
+        document.getElementById("fundDatetime").value = date;
+    }
+    function formatDate(date) {
+        var day = ('0' + date.getDate()).slice(-2);
+        var month = ('0' + (date.getMonth() + 1)).slice(-2);
+        var year = date.getFullYear();
+
+        return year + '-' + month + '-' + day;
+    }
+}
