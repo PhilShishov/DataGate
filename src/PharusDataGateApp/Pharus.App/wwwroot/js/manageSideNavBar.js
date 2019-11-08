5faeddc92932fa8b8c91bde653285d18ed7cee51
Philip Shishov <philshishov@gmail.com>
2019-11-08 15:43:53 +0100

    Refactoring JS code, modified onclick, onfocus with addeventlistener

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSideNavBar.js
@@ -0,0 +1,32 @@
+﻿function manageSideNavBar() {
+    const sideNavIcon = document.getElementById("sideNavIcon");
+    sideNavIcon.addEventListener('click', openNav);
+
+    const sideNav = document.getElementById("mySidenav");
+    sideNav.addEventListener('mouseleave', closeNav);
+
+    const closebtn = document.getElementById("closeBtn");
+    closebtn.addEventListener('click', closeNav);
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+        sideNav.style.width = "0";
+    }
+
+    const dropdown = document.getElementsByClassName("dropdown-btn");
+
+    for (let i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
+    }
+}
