f428b392c0efc7d743d98666b476fe0a8ecc3d18
Philip Shishov <philshishov@gmail.com>
2019-11-29 13:45:46 +0100

    Implemented view for CreateFundBindingModel

---
+++ src/PharusDataGateApp/Pharus.App/Models/BindingModels/Funds/EditFundBindingModel.cs
@@ -0,0 +1,30 @@
+﻿// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+// Model class for edit fund
+
+// Created: 10/2019
+// Author:  Philip Shishov
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+namespace Pharus.App.Models.BindingModels.Funds
+{
+    using System;
+
+    public class EditFundBindingModel : BaseEntityBindingModel
+    {
+        public DateTime ChosenDate { get; set; }
+
+        public int FId { get; set; }
+
+        public string FStatus { get; set; }
+
+        public string LegalForm { get; set; }
+
+        public string LegalVehicle { get; set; }
+
+        public string LegalType { get; set; }
+
+        public string CompanyTypeDesc { get; set; }
+
+        public string CompanyAcronym { get; set; }
+    }
+}
---
+++ src/PharusDataGateApp/Pharus.App/Utilities/GetAllActiveEntitiesUtility.cs
@@ -0,0 +1,91 @@
+﻿// Utility class for retrieving Active table data
+
+// Created: 11/2019
+// Author:  Philip Shishov
+
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+namespace Pharus.App.Utilities
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Pharus.Services.Contracts;
+    using Pharus.App.Models.ViewModels.Entities;
+
+    // _____________________________________________________________
+    public class GetAllActiveEntitiesUtility
+    {
+        // ________________________________________________________
+        //
+        // Empty and refill funds model
+        // with headers and correct data
+        // based on "Active" condition
+        public static void GetAllActiveFundsWithHeaders(EntitiesViewModel model, IFundsService fundsService)
+        {
+            model.Entities = new List<string[]>();
+
+            var tableHeaders = fundsService
+                .GetAllActiveFunds()
+                .Take(1)
+                .ToList();
+            var tableFundsWithoutHeaders = fundsService
+                .GetAllActiveFunds()
+                .Skip(1)
+                .ToList()
+                .Where(f => f.Contains("Active"))
+                .ToList();
+
+            model.Entities.AddRange(tableHeaders);
+            model.Entities.AddRange(tableFundsWithoutHeaders);
+        }       
+
+        // ________________________________________________________
+        //
+        // Empty and refill subfunds model
+        // with headers and correct data
+        // based on "Active" condition
+        public static void GetAllActiveSubFundsWithHeaders(EntitiesViewModel model, ISubFundsService subFundsService)
+        {
+            model.Entities = new List<string[]>();
+
+            var tableHeaders = subFundsService
+                .GetAllActiveSubFunds()
+                .Take(1)
+                .ToList();
+            var tableFundsWithoutHeaders = subFundsService
+                .GetAllActiveSubFunds()
+                .Skip(1)
+                .ToList()
+                .Where(f => f.Contains("Active"))
+                .ToList();
+
+            model.Entities.AddRange(tableHeaders);
+            model.Entities.AddRange(tableFundsWithoutHeaders);
+        }
+
+        // ________________________________________________________
+        //
+        // Empty and refill shareclasses model
+        // with headers and correct data
+        // based on "Active" condition
+        public static void GetAllActiveShareClassesWithHeaders(EntitiesViewModel model, IShareClassesService shareClassesService)
+        {
+            model.Entities = new List<string[]>();
+
+            var tableHeaders = shareClassesService
+                .GetAllActiveShareClasses()
+                .Take(1)
+                .ToList();
+            var tableFundsWithoutHeaders = shareClassesService
+                .GetAllActiveShareClasses()
+                .Skip(1)
+                .ToList()
+                .Where(f => f.Contains("Active"))
+                .ToList();
+
+            model.Entities.AddRange(tableHeaders);
+            model.Entities.AddRange(tableFundsWithoutHeaders);
+        }
+    }
+}
