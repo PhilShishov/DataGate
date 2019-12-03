08796dcc0876c8bcc9c1a25c96aa613a8ee34488
Philip Shishov <philshishov@gmail.com>
2019-12-03 16:52:32 +0100

    Created manage dropdowns with JS

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSideNavBar.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSideNavBar.js
@@ -14,31 +14,5 @@
 
     function closeNav() {
         sideNav.style.width = "0";
-    }
-
-    const dropdowns = document.getElementsByClassName("dropdown-btn");
-
-    for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            let dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === "block") {
-                dropdownContent.style.display = "none";
-            } else {
-                dropdownContent.style.display = "block";
-            }
-        });
-    }
-
-    const sideNavDropdowns = document.getElementById("");
-
-    sideNavDropdowns.addEventListener("mouseover", function () {
-        this.classList.toggle("active");
-        let dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block";
-        }
-    });
+    }  
 }
