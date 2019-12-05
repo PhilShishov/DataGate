013b0265d076bc497a412b00b2a8567a89bd4565
Philip Shishov <philshishov@gmail.com>
2019-12-05 11:31:10 +0100

    Fixed window.onload open select menu options Replaced 'on' events with addeventlistener

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSubEntitiesSelectMenu.js
@@ -0,0 +1,20 @@
+﻿function manageSESelectMenu() {
+    const selectMenu = document.getElementById('fundChoicesSelect');
+
+    const searchFieldSEDiv = document.getElementById('searchFieldDiv');
+
+    const extractSEDiv = document.getElementById('extractDiv');
+
+    selectMenu.addEventListener('change', openSelectMenu);
+
+    function openSelectMenu() {
+        if (selectMenu.options[selectMenu.selectedIndex].text === 'SubFunds') {
+            searchFieldSEDiv.removeAttribute('hidden');
+            extractSEDiv.removeAttribute('hidden');
+        }
+        else {
+            searchFieldSEDiv.setAttribute('hidden', true);
+            extractSEDiv.setAttribute('hidden', true);
+        }
+    }
+}
