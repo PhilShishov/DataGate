77191b91aef3bf264dd08142909823a52b9f85a9
Philip Shishov <philshishov@gmail.com>
2019-12-05 11:43:19 +0100

    After subfunds select menu option is clicked a new menu with search and extract options is available

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSubEntitiesSelectMenu.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/manageSubEntitiesSelectMenu.js
@@ -1,20 +1,23 @@
 ﻿function manageSESelectMenu() {
-    const selectMenu = document.getElementById('fundChoicesSelect');
+    const selectMenu = document.getElementsByName('fundChoicesSelect')[0];
 
     const searchFieldSEDiv = document.getElementById('searchFieldDiv');
 
-    const extractSEDiv = document.getElementById('extractDiv');
+    const extractBtnExl = document.getElementById('extractBtnExl');
+    const extractBtnPdf = document.getElementById('extractBtnPDF');
 
     selectMenu.addEventListener('change', openSelectMenu);
 
     function openSelectMenu() {
         if (selectMenu.options[selectMenu.selectedIndex].text === 'SubFunds') {
             searchFieldSEDiv.removeAttribute('hidden');
-            extractSEDiv.removeAttribute('hidden');
+            extractBtnExl.removeAttribute('hidden');
+            extractBtnPdf.removeAttribute('hidden');
         }
         else {
             searchFieldSEDiv.setAttribute('hidden', true);
-            extractSEDiv.setAttribute('hidden', true);
+            extractBtnExl.setAttribute('hidden', true);
+            extractBtnPdf.setAttribute('hidden', true);
         }
     }
 }
