8bdaebfcdd9bd95ee11be231c94663f611e92a91
Philip Shishov <philshishov@gmail.com>
2019-12-06 15:34:27 +0100

    Input with capital letter JS function created

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/inputCaps.js
@@ -0,0 +1,10 @@
+﻿function inputCaps() {
+
+    const fundNameInput = document.getElementById('fundNameInput');
+
+    fundNameInput.addEventListener('blur', caps);
+
+    function caps() {
+        this.value = fundNameInput.value.toUpperCase();
+    }
+}
