bba8eb6eecd4bebb88002d1d68f17a843457c4c6
Philip Shishov <philshishov@gmail.com>
2019-12-11 15:35:46 +0100

    Fund file uploaded with query to DB

--- src/PharusDataGateApp/Pharus.Services/Funds/FundsFileService.cs
+++ src/PharusDataGateApp/Pharus.Services/Funds/FundsFileService.cs
@@ -3,7 +3,7 @@
     using System;
     using System.Data;
     using System.Data.SqlClient;
-
+    using System.Data.SqlTypes;
     using Microsoft.Extensions.Configuration;
 
     using Pharus.Services.Funds.Contracts;
@@ -83,16 +83,14 @@
             }
         }
 
-        public void InsertFundFile(
+        public void AddFileToSpecificFund(
                                     string streamId,
                                     int fundId,
                                     string startConnection,
                                     string endConnection,
                                     int fileTypeId)
         {
-            string query = "insert into [dbo].[tb_map_filefund] values " +
-                "(file_stream_id, fund_id, startConnection, " +
-                "endConnection, filetype_id)";
+            string query = "EXEC sp_insert_map_fund @stream_id, @fund_id, @start_connection, @end_connection, @filetype_id";
 
             using (SqlConnection connection = new SqlConnection())
             {
@@ -101,11 +99,11 @@
                 {
                     command.Parameters.AddRange(new[]
                     {
-                        new SqlParameter("@file_stream_id", SqlDbType.NVarChar, 100) { Value = streamId },
+                        new SqlParameter("@stream_id", SqlDbType.NVarChar) { Value = streamId },
                         new SqlParameter("@fund_id", SqlDbType.Int) { Value = fundId },
-                        new SqlParameter("@startConnection", SqlDbType.NVarChar) { Value = startConnection },
-                        new SqlParameter("@endConnection", SqlDbType.NVarChar, 100) { Value = endConnection },
-                        new SqlParameter("@filetype_id", SqlDbType.Int) { Value = fileTypeId},
+                        new SqlParameter("@start_connection", SqlDbType.NVarChar, 100) { Value = startConnection },
+                        new SqlParameter("@end_connection", SqlDbType.NVarChar, 100) { Value = endConnection },
+                        new SqlParameter("@filetype_id", SqlDbType.Int) { Value = fileTypeId },
                     });
 
                     foreach (SqlParameter parameter in command.Parameters)
@@ -121,7 +119,7 @@
                     try
                     {
                         command.Connection.Open();
-                        command.ExecuteReader();
+                        command.ExecuteScalar();
                     }
                     catch (SqlException sx)
                     {
