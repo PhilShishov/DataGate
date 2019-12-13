f4af135375c3819215085b1299c4840e0dae49b1
Philip Shishov <philshishov@gmail.com>
2019-12-13 15:31:33 +0100

    SubFund main view functions implemented

--- src/PharusDataGateApp/Pharus.Services/SubFunds/Contracts/ISubFundsService.cs
+++ src/PharusDataGateApp/Pharus.Services/SubFunds/Contracts/ISubFundsService.cs
@@ -5,20 +5,66 @@
 
     public interface ISubFundsService
     {
-        List<string[]> GetAllActiveSubFunds();
+        List<string[]> GetAllSubFunds();
 
-        List<string[]> GetAllActiveSubFunds(DateTime? chosenDate);
+        List<string[]> GetAllSubFunds(DateTime? chosenDate);
 
-        List<string[]> GetActiveSubFundById(int id);
+        List<string> GetAllSubFundsNames();
 
-        List<string[]> GetActiveSubFundById(DateTime? chosenDate, int id);
+        List<string[]> GetSubFundById(int id);
 
-        List<string[]> GetActiveSubFundWithDateById(int id);
+        List<string[]> GetSubFundById(DateTime? chosenDate, int id);
+
+        List<string[]> GetSubFundWithDateById(int id);
+
+        List<string[]> GetSubFundWithDateById(DateTime? chosenDate, int id);
 
         List<string[]> GetSubFund_ShareClasses(int id);
+
+        List<string[]> GetSubFund_ShareClasses(DateTime? chosenDate, int id);
 
         List<string[]> GetSubFund_FundContainer(int id);
 
         List<string[]> GetSubFund_FundContainer(DateTime? chosenDate, int id);
+
+        List<string[]> GetSubFundTimeline(int id);
+
+        List<string[]> GetAllSubFundDocumens(int id);
+
+        void EditSubFund(
+                        int fundId,
+                        string initialDate,
+                        int fStatusId,
+                        string regNumber,
+                        string fundName,
+                        string leiCode,
+                        string cssfCode,
+                        string faCode,
+                        string depCode,
+                        string taCode,
+                        int fLegalFormId,
+                        int fLegalTypeId,
+                        int fLegalVehicleId,
+                        int fCompanyTypeId,
+                        string tinNumber,
+                        string comment,
+                        string commentTitle);
+
+        void CreateSubFund(
+                        string initialDate,
+                        string endDate,
+                        string fundName,
+                        string cssfCode,
+                        int fStatusId,
+                        int fLegalFormId,
+                        int fLegalTypeId,
+                        int fLegalVehicleId,
+                        string faCode,
+                        string depCode,
+                        string taCode,
+                        int fCompanyTypeId,
+                        string tinNumber,
+                        string leiCode,
+                        string regNumber);
     }
 }
