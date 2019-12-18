e62278adb36ff533e766d6dc590e5dc2e05069c7
Philip Shishov <philshishov@gmail.com>
2019-12-18 11:45:12 +0100

    Logo changed

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/formatInput.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/formatInput.js
@@ -3,10 +3,10 @@
     const createForm = document.getElementById('createForm').getElementsByTagName('input');
 
     for (var i = 0; i < createForm.length; i++) {
-        createForm[i].addEventListener('blur', caps);
-    }    
+        createForm[i].addEventListener('blur', format);
+    }
 
-    function caps() {
+    function format() {
         this.value = this.value.trimStart();
         this.value = this.value.trimEnd();
         this.value = this.value.toUpperCase();
