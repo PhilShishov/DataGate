2b8ab31899ea3dab4e845a46ab7429bca7fa3395
Philip Shishov <philshishov@gmail.com>
2019-12-19 16:14:01 +0100

    Active switch attached to submit action

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/checkboxRedirect.js
@@ -0,0 +1,9 @@
+﻿function checkboxRedirect() {
+    const checkbox = document.getElementById('activeCheckBox');
+
+    checkbox.addEventListener('change', submitForm)
+
+    function submitForm() {
+        this.form.submit();
+    }
+}
---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/rowRedirect.js
@@ -0,0 +1,15 @@
+﻿function rowRedirect(controllerName, entityId, chosenDate) {
+    const tableRow = document.getElementsByClassName('tableRowPharus');
+
+    console.log(controllerName);
+    console.log(entityId);
+    console.log(chosenDate);
+
+    for (const row of tableRow) {
+        row.addEventListener('click', redirect);
+    }
+
+    function redirect() {
+        document.location = `/${controllerName}/ViewEntitySE/${entityId}/${chosenDate}`;
+    }
+}
