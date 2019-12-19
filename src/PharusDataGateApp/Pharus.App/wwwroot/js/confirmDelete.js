abf814be82af05dff518f5cadbfb3529231cd4f5
Philip Shishov <philshishov@gmail.com>
2019-12-19 12:56:57 +0100

    All confirm dialogs js passed to addeventlistener

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/confirmDelete.js
@@ -0,0 +1,8 @@
+﻿function confirmDelete() {
+    const confrDelBtn = document.getElementById('confirmDelBtn');
+    confrDelBtn.addEventListener('click', showDeleteDialog);
+
+    function showDeleteDialog() {
+        confirm('Are you sure you want to delete this?');
+    }  
+}
