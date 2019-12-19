14279cfe65cdd0da6b00f04d061b48dda544a74e
Philip Shishov <philshishov@gmail.com>
2019-12-19 12:42:00 +0100

    onclick for confirm dialog refactored to addeventlistener

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/confirmEvent.js
@@ -0,0 +1,9 @@
+﻿function confirmEvent() {
+    const confrBtn = document.getElementById('confirmBtn');
+
+    confrBtn.addEventListener('click', showConfirmDialog);
+
+    function showConfirmDialog() {
+        confirm('Are you sure you want to delete this?');
+    }
+}
