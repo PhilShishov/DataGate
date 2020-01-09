6cd842aed05621f2627d04e7eae94814238039e1
Philip Shishov <philshishov@gmail.com>
2020-01-09 14:36:08 +0100

    Extract pdf for fund subfunds corrected

--- src/PharusDataGateApp/Pharus.Services/Funds/Contracts/IFundsService.cs
+++ src/PharusDataGateApp/Pharus.Services/Funds/Contracts/IFundsService.cs
@@ -39,6 +39,8 @@
 
         List<string[]> GetAllFundDocumens(int id);
 
+        List<string[]> PrepareFund_SubFundsForPDFExtract(DateTime? chosenDate);
+
         void EditFund(
                         int fundId,
                         string initialDate,
