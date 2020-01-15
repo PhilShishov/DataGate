3cad93767b69e7bacd2c98577dd7d65c34ebc8a2
Philip Shishov <philshishov@gmail.com>
2020-01-15 09:59:01 +0100

    Correct format for Sub fund and share class official names

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/formatInput.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/formatInput.js
@@ -1,13 +1,32 @@
 ﻿function formatInput() {
-    const createForm = document.getElementById('createForm').getElementsByTagName('input');
+    const createFormInputField = document.getElementById('createForm').querySelectorAll('input:not(#formatName)');
+    const formatName = document.getElementById('formatName');
 
-    for (var i = 0; i < createForm.length; i++) {
-        createForm[i].addEventListener('blur', format);
+    for (var i = 0; i < createFormInputField.length; i++) {
+        createFormInputField[i].addEventListener('blur', formatInput);
     }
 
-    function format() {
+    formatName.addEventListener('blur', formatSubFundAndShareClassName)
+
+    function formatInput() {
         this.value = this.value.trimStart();
         this.value = this.value.trimEnd();
         this.value = this.value.toUpperCase();
+    }
+
+    function formatSubFundAndShareClassName() {
+        this.value = this.value.trimStart();
+        this.value = this.value.trimEnd();
+        this.value = capitalLetter(this.value);
+
+        function capitalLetter(str) {
+            str = str.split(" ");
+
+            for (let i = 0, x = str.length; i < x; i++) {
+                str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+            }
+
+            return str.join(" ");
+        }
     }
 }
