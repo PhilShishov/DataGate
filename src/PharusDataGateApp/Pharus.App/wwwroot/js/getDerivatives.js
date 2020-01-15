cdc923df8031f94c215327d424f433085828d565
Philip Shishov <philshishov@gmail.com>
2020-01-15 09:31:00 +0100

    Fixed derivatives show menu if selected option is yes

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/getDerivatives.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/getDerivatives.js
@@ -4,14 +4,21 @@
     const derivMarket = document.getElementById('derivMarket');
     const derivPurpose = document.getElementById('derivPurpose');
 
+    if (deriv.value == 'Yes') {
+        derivMarket.removeAttribute('hidden');
+        derivPurpose.removeAttribute('hidden');
+    } else {
+        derivMarket.setAttribute('hidden', true);
+        derivPurpose.setAttribute('hidden', true);
+    }
+
     deriv.addEventListener('change', openDeriv);
 
     function openDeriv() {
         if (deriv.options[deriv.selectedIndex].text === 'Yes') {
             derivMarket.removeAttribute('hidden');
             derivPurpose.removeAttribute('hidden');
-        }
-        else {
+        } else {
             derivMarket.setAttribute('hidden', true);
             derivPurpose.setAttribute('hidden', true);
         }
