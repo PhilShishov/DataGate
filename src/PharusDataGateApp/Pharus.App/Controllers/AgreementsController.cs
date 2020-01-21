f1501122d5cafd8d864e19c6d9e4b9caf3b9db25
Philip Shishov <philshishov@gmail.com>
2020-01-21 11:27:06 +0100

    Created a new Agreements controller to display data about all the agreements

---
+++ src/PharusDataGateApp/Pharus.App/Controllers/AgreementsController.cs
@@ -0,0 +1,42 @@
+﻿namespace Pharus.App.Controllers
+{
+    using System;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Pharus.App.Models.ViewModels.Agreements;
+    using Pharus.Services.Funds.Contracts;
+    using Pharus.Services.SubFunds.Contracts;
+    using Pharus.Services.ShareClasses.Contracts;
+
+    public class AgreementsController : Controller
+    {
+        private readonly IFundsService fundsService;
+        private readonly ISubFundsService subFundsService;
+        private readonly IShareClassesService shareClassesService;
+
+        public AgreementsController(
+            IFundsService fundsService,
+            ISubFundsService subFundsService,
+            IShareClassesService shareClassesService)
+        {
+            this.fundsService = fundsService;
+            this.subFundsService = subFundsService;
+            this.shareClassesService = shareClassesService;
+        }
+
+        public IActionResult All()
+        {
+            var model = new AgreementsViewModel
+            {
+                ChosenDate = DateTime.Today.ToString("yyyy-MM-dd"),
+            };
+
+            var chosenDate = DateTime.Parse(model.ChosenDate);
+
+            model.EntityDocuments = this.fundsService.GetAllAgreementDocumentsForAllFunds(chosenDate);
+
+            return View(model);
+        }
+    }
+}
---
+++ src/PharusDataGateApp/Pharus.App/Models/ViewModels/Agreements/AgreementsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Pharus.App.Models.ViewModels.Agreements
+{
+    using System.Collections.Generic;
+
+    public class AgreementsViewModel
+    {
+        public string ChosenDate { get; set; }
+
+        public string SelectedItem { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        public string Command { get; set; }
+
+        public List<string[]> EntityDocuments { get; set; }
+    }
+}
