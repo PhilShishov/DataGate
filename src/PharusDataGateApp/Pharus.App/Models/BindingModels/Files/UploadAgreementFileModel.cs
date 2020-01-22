feba782b68a845774aabaee24205963f2b8bbb2e
Philip Shishov <philshishov@gmail.com>
2020-01-22 09:43:32 +0100

    Model for agreement upload prepared

--- src/PharusDataGateApp/Pharus.App/Models/BindingModels/Files/UploadAgreementFileModel.cs
+++ src/PharusDataGateApp/Pharus.App/Models/BindingModels/Files/UploadAgreementFileModel.cs
@@ -7,12 +7,33 @@
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 namespace Pharus.App.Models.BindingModels.Files
 {
-    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.ComponentModel.DataAnnotations;
 
-    public class UploadAgreementFileModel
+    public class UploadAgreementFileModel : BaseUploadFileBindingModel
     {
-        public IFormFile FileToUpload { get; set; }
+        [Required]
+        [Display(Name = "Agreement Type")]
+        public string AgrType { get; set; }
 
-        public string FileType { get; set; }
+        [Required(ErrorMessage = "Contract Date cannot be empty")]
+        [Display(Name = "Contract Date")]
+        public DateTime ContractDate { get; set; }
+
+
+        [Required(ErrorMessage = "Activation Date cannot be empty")]
+        [Display(Name = "Contract Date")]
+        public DateTime ActivationDate { get; set; }
+
+        [Display(Name = "Expiration Date")]
+        public DateTime? ExpirationDate { get; set; }
+
+        [Required]
+        [Display(Name = "Company")]
+        public string Company { get; set; }
+
+        [Required]
+        [Display(Name = "Status")]
+        public string Status { get; set; }
     }
 }
