f2202b207fa857fcacd32d5cebcb1dd871009061
Philip Shishov <philshishov@gmail.com>
2020-01-28 12:40:49 +0100

    agreement is deleted only if ok button on confirm box is selected

--- src/PharusDataGateApp/Pharus.App/wwwroot/js/deleteAgreement.js
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/deleteAgreement.js
@@ -1,21 +1,28 @@
 ﻿function passUrlToDelete(url) {
     $(".confirmDelBtn").on('click', function () {
-        const id = $(this).closest('tr').find("td:eq(0)").text().trim();
-        const agrName = $(this).closest('tr').find("td:eq(2)").text().trim();
-        $.ajax({
-            type: "GET",
-            url: url,
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            data: { id: id, agrName: agrName },
-            success: function (data) {
-                if (data.data == "false") {
-                    alert("Something wrong!");
-                    return false;
-                }
-                $("#tbl-distinct-agreements tr:contains('" + data.data + "')").remove();
-            },
-            async: false
-        });
+        let con = confirm('Are you sure you want to delete this?');
+
+        if (!con) {
+            event.preventDefault();
+        }
+        else {
+            const id = $(this).closest('tr').find("td:eq(0)").text().trim();
+            const agrName = $(this).closest('tr').find("td:eq(2)").text().trim();
+            $.ajax({
+                type: "GET",
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: { id: id, agrName: agrName },
+                success: function (data) {
+                    if (data.data == "false") {
+                        alert("Something wrong!");
+                        return false;
+                    }
+                    $("#tbl-distinct-agreements tr:contains('" + data.data + "')").remove();
+                },
+                async: false
+            });
+        }
     });
 }
