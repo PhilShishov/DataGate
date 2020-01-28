af9495a73676d3a8c66a7ce990e7e7cb73918e1b
Philip Shishov <philshishov@gmail.com>
2020-01-28 18:13:32 +0100

    Delete document jquery

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/delete/deleteDocument.js
@@ -0,0 +1,28 @@
+﻿function passDocUrlToDelete(url) {
+    $(".confirmDocDelBtn").on('click', function () {
+        let con = confirm('Are you sure you want to delete this?');
+
+        if (!con) {
+            event.preventDefault();
+        }
+        else {
+            const id = $(this).closest('tr').find("td:eq(0)").text().trim();
+            const agrName = $(this).closest('tr').find("td:eq(2)").text().trim();
+            $.ajax({
+                type: "GET",
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: { id: id, agrName: agrName },
+                success: function (data) {
+                    if (data.data == "false") {
+                        alert("Something wrong!");
+                        return false;
+                    }
+                    $("#tbl-distinct-documents tr:contains('" + data.data + "')").remove();
+                },
+                async: false
+            });
+        }
+    });
+}
