239a09b16f4c9a67b4a09c6e5447a51447bb65eb
Philip Shishov <philshishov@gmail.com>
2020-01-28 12:21:22 +0100

    jQuery code for delete agreement moved to external file

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/deleteAgreement.js
@@ -0,0 +1,21 @@
+﻿function passUrlToDelete(url) {
+    $(".confirmDelBtn").on('click', function () {
+        const id = $(this).closest('tr').find("td:eq(0)").text().trim();
+        const agrName = $(this).closest('tr').find("td:eq(2)").text().trim();
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: { id: id, agrName: agrName },
+            success: function (data) {
+                if (data.data == "false") {
+                    alert("Something wrong!");
+                    return false;
+                }
+                $("#tbl-distinct-agreements tr:contains('" + data.data + "')").remove();
+            },
+            async: false
+        });
+    });
+}
