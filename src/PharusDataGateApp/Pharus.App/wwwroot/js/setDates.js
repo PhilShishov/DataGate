7d3d075637b5c0c85e6ad924590a40306454df4e
Philip Shishov <philshishov@gmail.com>
2020-01-28 17:24:47 +0100

    Set ActivationDate js for upload agreement

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/setDates.js
@@ -0,0 +1,12 @@
+﻿function setDates() {
+    const contractDate = document.getElementById('contractDate');
+    const activationDate = document.getElementById('activationDate');
+
+    contractDate.addEventListener('change', setActivationDate);
+    //activationDate.addEventListener('change', setContractDate);
+
+    function setActivationDate() {
+        activationDate.setAttribute('min', contractDate.value);
+        activationDate.value = contractDate.value;
+    }
+}
