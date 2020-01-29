4dec19f30db3699bc90717783fc0096935abcc32
Philip Shishov <philshishov@gmail.com>
2020-01-29 14:34:35 +0100

    select additional info for share class js

---
+++ src/PharusDataGateApp/Pharus.App/wwwroot/js/select-additional-info/select-shareclass-addInfo.js
@@ -0,0 +1,55 @@
+﻿// ________________________________________________________
+//
+// Select menu for fund additional information -
+// subentities, timeline changes, all documents
+// Get selected value and keep select option open
+
+$('#shareClassAdditionalInfSelect option').each(function () {
+    if (this.selected) {
+        const dropdownvalue = $("#shareClassAdditionalInfSelect option:selected").val();
+        if (dropdownvalue == 'TimelineChanges') {
+            $("#timelineChanges").css('visibility', 'visible');
+            $("#allDocuments").addClass('d-none');
+            $("#allAgreements").addClass('d-none');
+        } else if (dropdownvalue == 'AllDocuments') {
+            $("#allDocuments").removeClass('d-none');
+            $("#timelineChanges").css('visibility', 'hidden');
+            $("#allAgreements").addClass('d-none');
+        } else if (dropdownvalue == 'AllAgreements') {
+            $("#allAgreements").removeClass('d-none');
+            $("#timelineChanges").css('visibility', 'hidden');
+            $("#allDocuments").addClass('d-none');
+        } else {
+            $("#timelineChanges").css('visibility', 'hidden');
+            $("#allDocuments").addClass('d-none');
+            $("#allAgreements").addClass('d-none');
+        }
+    }
+});
+
+// ________________________________________________________
+//
+// Select menu for fund additional information -
+// subentities, timeline changes, all documents -
+// On change event for selected value
+
+$("#shareClassAdditionalInfSelect").on('change', function () {
+    const dropdownvalue = $("#shareClassAdditionalInfSelect option:selected").val();
+    if (dropdownvalue == 'TimelineChanges') {
+        $("#timelineChanges").css('visibility', 'visible');
+        $("#allDocuments").addClass('d-none');
+        $("#allAgreements").addClass('d-none');
+    } else if (dropdownvalue == 'AllDocuments') {
+        $("#allDocuments").removeClass('d-none');
+        $("#timelineChanges").css('visibility', 'hidden');
+        $("#allAgreements").addClass('d-none');
+    } else if (dropdownvalue == 'AllAgreements') {
+        $("#allAgreements").removeClass('d-none');
+        $("#timelineChanges").css('visibility', 'hidden');
+        $("#allDocuments").addClass('d-none')
+    } else {
+        $("#timelineChanges").css('visibility', 'hidden');
+        $("#allDocuments").addClass('d-none');
+        $("#allAgreements").addClass('d-none');
+    }
+});
