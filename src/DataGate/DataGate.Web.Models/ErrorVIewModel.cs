8adf4d1a1144b251f62aee124d59b5d1c1de471c
Philip Shishov <philshishov@gmail.com>
2020-04-27 08:01:37 +0200

    Removed files from old project

--- src/DataGate/DataGate.Web.Models/ErrorVIewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataGate.Web.Models.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
