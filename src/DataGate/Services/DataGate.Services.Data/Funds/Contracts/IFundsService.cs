e8f48b186db4aa2922a46606c0782ed3e46ff0c0
Philip Shishov <philshishov@gmail.com>
2020-04-28 00:11:00 +0200

    Changed List to IEnumerable for types that are only read

---
+++ src/DataGate/Services/DataGate.Services.Data/Funds/Contracts/IFundsService.cs
@@ -0,0 +1,84 @@
+﻿namespace DataGate.Services.Data.Funds.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IFundsService
+    {
+        IEnumerable<string[]> GetAllFunds(DateTime? chosenDate);
+
+        IEnumerable<string[]> GetAllActiveFunds();
+
+        IEnumerable<string[]> GetAllActiveFunds(DateTime? chosenDate);
+
+        IEnumerable<string[]> GetAllFundsWithSelectedViewAndDate(
+                        List<string> preSelectedColumns, 
+                        List<string> selectedColumns, 
+                        DateTime? chosenDate);
+
+        IEnumerable<string[]> GetAllActiveFundsWithSelectedViewAndDate(
+                        List<string> preSelectedColumns, 
+                        List<string> selectedColumns, 
+                        DateTime? chosenDate);
+
+        //IEnumerable<string> GetAllFundsNames();
+
+        IEnumerable<string[]> GetFundWithDateById(DateTime? chosenDate, int id);
+
+        IEnumerable<string[]> GetFund_SubFunds(DateTime? chosenDate, int id);
+
+        IEnumerable<string[]> GetFund_SubFundsWithSelectedViewAndDate(
+                        List<string> preSelectedColumns, 
+                        List<string> selectedColumns, 
+                        DateTime? chosenDate, 
+                        int id);
+
+        IEnumerable<string[]> GetFundTimeline(int id);
+
+        IEnumerable<string[]> GetDistinctFundDocuments(DateTime? chosenDate, int id);
+
+        IEnumerable<string[]> GetAllFundDocuments(int id);
+
+        IEnumerable<string[]> GetDistinctFundAgreements(DateTime? chosenDate, int id);
+
+        IEnumerable<string[]> GetAllFundAgreements(DateTime? chosenDate, int id);
+
+        IEnumerable<string[]> PrepareFund_SubFundsForPDFExtract(DateTime? chosenDate);
+
+        void EditFund(
+                        int fundId,
+                        string initialDate,
+                        int fStatusId,
+                        string regNumber,
+                        string fundName,
+                        string leiCode,
+                        string cssfCode,
+                        string faCode,
+                        string depCode,
+                        string taCode,
+                        int fLegalFormId,
+                        int fLegalTypeId,
+                        int fLegalVehicleId,
+                        int fCompanyTypeId,
+                        string tinNumber,
+                        string comment,
+                        string commentTitle);
+
+        void CreateFund(
+                        string initialDate,
+                        string endDate,
+                        string fundName,
+                        string cssfCode,
+                        int fStatusId,
+                        int fLegalFormId,
+                        int fLegalTypeId,
+                        int fLegalVehicleId,
+                        string faCode,
+                        string depCode,
+                        string taCode,
+                        int fCompanyTypeId,
+                        string tinNumber,
+                        string leiCode,
+                        string regNumber);
+    }
+}
