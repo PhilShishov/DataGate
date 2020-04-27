944a04a48646553a652f04d4d92be8016664524d
Philip Shishov <philshishov@gmail.com>
2020-04-27 08:19:19 +0200

    Removed for gitignore update

--- src/DataGate/Web/DataGate.Web/wwwroot/js/buttonRedirect.js
+++
@@ -1,9 +0,0 @@
-﻿function buttonRedirect() {
-    const btn = document.getElementById('btnRedirect');
-
-    btn.addEventListener('click', redirect);
-
-    function redirect() {
-        window.location.href = 'All';
-    }
-}
