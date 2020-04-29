5398c52c6a42c7b89c3363dbcf849b2dc6ba0575
Philip Shishov <philshishov@gmail.com>
2020-04-29 12:00:58 +0200

    New controller and services for fund subfunds

---
+++ src/DataGate/Services/DataGate.Services.Data/FundSubFunds/FundSubFundsService.cs
@@ -0,0 +1,95 @@
+namespace DataGate.Services.Data.FundSubFunds
+{
+    using DataGate.Common;
+    using DataGate.Services.Data.FundSubFunds.Contracts;
+    using DataGate.Services.SqlClient;
+    using Microsoft.Extensions.Configuration;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    public class FundSubFundsService : IFundSubFundsService
+    {
+        // ________________________________________________________
+        //
+        // Table functions names as in DB
+        private readonly string sqlFunctionAllFund = "fn_all_fund";
+        private readonly string sqlFunctionAllActiveFund = "fn_active_fund";
+        private readonly string sqlFunctionSubFundPdfView = "fn_active_subfund_pdf";
+        private readonly string sqlFunctionTimelineFund = "dbo.fn_timeline_fund";
+        private readonly string sqlFunctionDistinctDocuments = "[dbo].[fn_view_distinct_documents_fund]";
+        private readonly string sqlFunctionAllDocuments = "[dbo].[fn_view_documents_fund]";
+        private readonly string sqlFunctionDistinctAgreements = "[dbo].[fn_view_distinct_agreements_fund]";
+        private readonly string sqlFunctionAllAgreements = "[dbo].[fn_view_agreements_fund]";
+        private readonly string sqlFunctionSubFundsForFund = "ActiveSubFundsForSpecificFundAtDate";
+
+        private readonly string columnToPassToQuery = "FUND ID PHARUS";
+        private readonly IConfiguration configuration;
+        private readonly ISqlQueryManager sqlManager;
+
+        // ________________________________________________________
+        //
+        // Constructor: initialize with DI IConfiguration
+        // to retrieve appsettings.json connection string
+        public FundSubFundsService(IConfiguration config, ISqlQueryManager sqlQueryManager)
+        {
+            this.configuration = config;
+            this.sqlManager = sqlQueryManager;
+        }
+
+        // ________________________________________________________
+        //
+        // Retrieve query table DB based entities
+        // with table functions
+
+        public IEnumerable<string[]> GetFundWithDateById(DateTime? chosenDate, int id)
+        {
+            return ExecuteSqlQueryByWhereId(chosenDate, id, this.sqlFunctionAllFund, this.columnToPassToQuery);
+        }
+
+        public IEnumerable<string[]> GetFund_SubFunds(DateTime? chosenDate, int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryByDateAndId(chosenDate, id, this.sqlFunctionSubFundsForFund);
+        }
+
+        public IEnumerable<string[]> GetFund_SubFundsWithSelectedViewAndDate(
+                                                                    List<string> preSelectedColumns,
+                                                                    IEnumerable<string> selectedColumns,
+                                                                    DateTime? chosenDate,
+                                                                    int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryByIdWithSelection(ref preSelectedColumns, selectedColumns, chosenDate, id, this.sqlFunctionSubFundsForFund);
+        }
+
+        public IEnumerable<string[]> GetFundTimeline(int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryById(id, this.sqlFunctionTimelineFund);
+        }
+
+        public IEnumerable<string[]> GetDistinctFundDocuments(DateTime? chosenDate, int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryByDateAndId(chosenDate, id, this.sqlFunctionDistinctDocuments);
+        }
+
+        public IEnumerable<string[]> GetAllFundDocuments(int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryById(id, this.sqlFunctionAllDocuments);
+        }
+
+        public IEnumerable<string[]> GetDistinctFundAgreements(DateTime? chosenDate, int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryByDateAndId(chosenDate, id, this.sqlFunctionDistinctAgreements);
+        }
+
+        public IEnumerable<string[]> GetAllFundAgreements(DateTime? chosenDate, int id)
+        {
+            return this.sqlManager.ExecuteSqlQueryByDateAndId(chosenDate, id, this.sqlFunctionAllAgreements);
+        }
+
+        public IEnumerable<string[]> PrepareFund_SubFundsForPDFExtract(DateTime? chosenDate)
+        {
+            return this.sqlManager.ExecuteSqlQuery(chosenDate, this.sqlFunctionSubFundPdfView);
+        }
+    }
+}
---
+++ src/DataGate/Web/DataGate.Web/Controllers/FundSubEntitiesController.cs
@@ -0,0 +1,98 @@
+﻿//namespace DataGate.Web.Controllers
+//{
+//    using System;
+//    using System.Globalization;
+//    using System.Linq;
+//    using DataGate.Services.Data.FundSubFunds.Contracts;
+//    using DataGate.Web.ViewModels.Entities;
+
+//    using Microsoft.AspNetCore.Authorization;
+//    using Microsoft.AspNetCore.Mvc;
+
+//    [Authorize]
+//    public class FundSubEntitiesController : BaseController
+//    {
+//        private readonly IFundSubFundsService fundsService;
+
+//        public FundSubEntitiesController(IFundSubFundsService fundSubFundsService)
+//        {
+//            this.fundsService = fundSubFundsService;
+//        }
+
+//        [HttpGet]
+//        //[Route("Funds/ViewEntitySE/{EntityId}/{ChosenDate}")]
+//        public IActionResult ByIdAndDate(int entityId, string chosenDate)
+//        {
+//            SpecificEntityViewModel viewModel = new SpecificEntityViewModel
+//            {
+//                ChosenDate = chosenDate,
+//                EntityId = entityId,
+//            };
+
+//            this.SetModelValuesForSpecificView(viewModel);
+
+//            //this.HttpContext.Session.SetString("entityId", Convert.ToString(entityId));
+
+//            this.ModelState.Clear();
+//            return this.View(viewModel);
+//        }
+
+//        //public JsonResult AutoCompleteSubFundList(string selectTerm, int entityId)
+//        //{
+//        //    var entitiesToSearch = this.fundsService
+//        //        .GetFund_SubFunds(null, entityId)
+//        //        .Skip(1)
+//        //        .ToList();
+
+//        //    if (selectTerm != null)
+//        //    {
+//        //        entitiesToSearch = entitiesToSearch.Where(sf => sf[3]
+//        //                                             .ToLower()
+//        //                                             .Contains(selectTerm
+//        //                                             .ToLower()))
+//        //                                           .ToList();
+//        //    }
+
+//        //    var modifiedData = entitiesToSearch.Select(sf => new
+//        //    {
+//        //        id = sf[3],
+//        //        text = sf[3],
+//        //    });
+
+//        //    return this.Json(modifiedData);
+//        //}
+
+//        private void SetModelValuesForSpecificView(SpecificEntityViewModel model)
+//        {
+//            model.ControllerName = this.ControllerContext.RouteData.Values["controller"].ToString();
+//            var date = DateTime.ParseExact(model.ChosenDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+//            int entityId = model.EntityId;
+
+//            model.Entity = this.fundsService.GetFundWithDateById(date, entityId).ToList();
+//            model.EntityDistinctDocuments = this.fundsService.
+//                GetDistinctFundDocuments(date, entityId).ToList();
+//            model.EntityDistinctAgreements = this.fundsService.GetDistinctFundAgreements(date, entityId).ToList();
+
+//            model.EntitySubEntities = this.fundsService.GetFund_SubFunds(date, entityId).ToList();
+//            model.EntitiesHeadersForColumnSelection = this.fundsService
+//                                                                .GetFund_SubFunds(date, entityId)
+//                                                                .Take(1)
+//                                                                .ToList();
+//            model.EntityTimeline = this.fundsService.GetFundTimeline(entityId).ToList();
+//            model.EntityDocuments = this.fundsService.GetAllFundDocuments(entityId).ToList();
+//            model.EntityAgreements = this.fundsService.GetAllFundAgreements(date, entityId).ToList();
+
+//            model.StartConnection = DateTime.ParseExact(model.Entity.ToList()[1][0], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+//            if (model.EndConnection != null)
+//            {
+//                model.EndConnection = DateTime.ParseExact(model.Entity.ToList()[1][1], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+//            }
+
+//            //this.ViewData["ProspectusFileTypes"] = this.fundsSelectListService.GetAllProspectusFileTypes();
+//            //this.ViewData["AgreementsFileTypes"] = this.fundsSelectListService.GetAllAgreementsFileTypes();
+//            //this.ViewData["AgreementsStatus"] = this.agreementsSelectListService.GetAllTbDomAgreementStatus();
+//            //this.ViewData["Companies"] = this.agreementsSelectListService.GetAllTbCompanies();
+//        }
+//    }
+//}
