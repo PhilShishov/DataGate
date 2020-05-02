d469766fcd373ab97488c3902fc6b19475bb90d0
Philip Shishov <philshishov@gmail.com>
2020-05-02 10:31:36 +0200

    Controller for extraction of pdf and excel reports

---
+++ src/DataGate/Web/DataGate.Web/Controllers/Files/ExtractionController.cs
@@ -0,0 +1,40 @@
+﻿namespace DataGate.Web.Controllers.Files
+{
+    using DataGate.Common;
+    using DataGate.Services.DateTime;
+    using DataGate.Web.Utilities;
+    using DataGate.Web.ViewModels.Entities;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ExtractionController : Controller
+    {
+        [HttpPost]
+        public IActionResult GenerateExcel(EntitiesViewModel model)
+        {
+            if (model.Count > GlobalConstants.RowNumberOfHeadersInTable)
+            {
+                string typeName = model.GetType().Name;
+
+                return GenerateFileTemplate.Excel(model.Entities, typeName, GlobalConstants.FundsControllerName);
+            }
+
+            this.TempData[GlobalConstants.ErrorMessageDisplay] = ErrorMessages.TableReportNotGenerated;
+            return this.Redirect(GlobalConstants.FundAllUrl);
+        }
+
+        [HttpPost]
+        public IActionResult GeneratePdf(EntitiesViewModel model)
+        {
+            if (model.Count > GlobalConstants.RowNumberOfHeadersInTable)
+            {
+                var chosenDate = DateTimeParser.WebFormat(model.ChosenDate);
+                string typeName = model.GetType().Name;
+
+                return GenerateFileTemplate.Pdf(model.Entities, chosenDate, typeName, GlobalConstants.FundsControllerName);
+            }
+
+            this.TempData[GlobalConstants.ErrorMessageDisplay] = ErrorMessages.TableReportNotGenerated;
+            return this.Redirect(GlobalConstants.FundAllUrl);
+        }
+    }
+}
--- src/DataGate/Web/DataGate.Web/Controllers/Files/UploadController.cs
+++ src/DataGate/Web/DataGate.Web/Controllers/Files/UploadController.cs
@@ -1,42 +1,9 @@
 ﻿namespace DataGate.Web.Controllers.Files
 {
-    using DataGate.Common;
-    using DataGate.Services.DateTime;
-    using DataGate.Web.Utilities;
-    using DataGate.Web.ViewModels.Entities;
     using Microsoft.AspNetCore.Mvc;
 
-    public class TransferController : Controller
+    public class UploadController : Controller
     {
-        [HttpPost]
-        public IActionResult GenerateExcel(EntitiesViewModel model)
-        {
-            if (model.Count > GlobalConstants.RowNumberOfHeadersInTable)
-            {
-                string typeName = model.GetType().Name;
-
-                return GenerateFileTemplate.Excel(model.Entities, typeName, GlobalConstants.FundsControllerName);
-            }
-
-            this.TempData[GlobalConstants.ErrorMessageDisplay] = ErrorMessages.TableReportNotGenerated;
-            return this.Redirect(GlobalConstants.FundAllUrl);
-        }
-
-        [HttpPost]
-        public IActionResult GeneratePdf(EntitiesViewModel model)
-        {
-            if (model.Count > GlobalConstants.RowNumberOfHeadersInTable)
-            {
-                var chosenDate = DateTimeParser.WebFormat(model.ChosenDate);
-                string typeName = model.GetType().Name;
-
-                return GenerateFileTemplate.Pdf(model.Entities, chosenDate, typeName, GlobalConstants.FundsControllerName);
-            }
-
-            this.TempData[GlobalConstants.ErrorMessageDisplay] = ErrorMessages.TableReportNotGenerated;
-            return this.Redirect(GlobalConstants.FundAllUrl);
-        }
-
         //<form method = "post" enctype="multipart/form-data" 
         //                    asp-controller="Files" asp-action="Upload">
         //    <input type = "file" name="file">    <button type = "submit" value="Upload" />
