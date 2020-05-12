dd7da0ce9855a7454864c07c39b8cd68b58a150d
Philip Shishov <philshishov@gmail.com>
2020-05-12 19:53:02 +0200

    Separate reset and update action

--- src/DataGate/Services/DataGate.Services/CreateTableView.cs
+++ src/DataGate/Services/DataGate.Services/CreateTableView.cs
@@ -10,7 +10,6 @@ namespace DataGate.Services
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
     // _____________________________________________________________
