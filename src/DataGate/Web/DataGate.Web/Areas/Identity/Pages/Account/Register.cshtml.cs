cca6b397d62d67bf948e4029e88743eea61a3500
Philip Shishov <philshishov@gmail.com>
2020-05-16 20:38:07 +0200

    Add empty identity register to redirect to app page for security

---
+++ src/DataGate/Web/DataGate.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,35 @@
+﻿namespace DataGate.Web.Areas.Identity.Pages.Account
+{
+    using DataGate.Common;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [Authorize]
+    public class RegisterModel : PageModel
+    {
+        public RegisterModel()
+        {
+        }
+
+        public IActionResult OnGet(string returnUrl = null)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.Redirect(GlobalConstants.FundAllUrl);
+            }
+
+            return this.Redirect(GlobalConstants.FundAllUrl);
+        }
+
+        public IActionResult OnPost(string returnUrl = null)
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.Redirect(GlobalConstants.FundAllUrl);
+            }
+
+            return this.Redirect(GlobalConstants.FundAllUrl);
+        }
+    }
+}
