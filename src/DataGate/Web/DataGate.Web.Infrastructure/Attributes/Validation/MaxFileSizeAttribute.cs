5d97938d943d89c7332cf2b82d4110635218a8ac
Philip Shishov <philshishov@gmail.com>
2020-05-22 23:24:22 +0200

    Two validation attributes for file upload

---
+++ src/DataGate/Web/DataGate.Web.Infrastructure/Attributes/Validation/MaxFileSizeAttribute.cs
@@ -0,0 +1,37 @@
+﻿namespace DataGate.Web.Infrastructure.Attributes.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Http;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int maxFileSize;
+
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            this.maxFileSize = maxFileSize;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is { this.maxFileSize} bytes.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > this.maxFileSize)
+                {
+                    return new ValidationResult(this.GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
