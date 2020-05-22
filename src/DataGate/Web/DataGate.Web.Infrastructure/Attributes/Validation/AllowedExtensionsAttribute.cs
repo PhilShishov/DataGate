71f2a193646f6dbc9b019f603476ade88c6b8ecd
Philip Shishov <philshishov@gmail.com>
2020-05-22 23:24:22 +0200

    Two validation attributes for file upload

---
+++ src/DataGate/Web/DataGate.Web.Infrastructure/Attributes/Validation/AllowedExtensionsAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace DataGate.Web.Infrastructure.Attributes.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Http;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] extensions;
+
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            this.extensions = extensions;
+        }
+
+        public string GetErrorMessage()
+        {
+            return $"This file extension is not allowed!";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+
+                if (!this.extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(this.GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
