2b710d4d1402fe2fca74645affb8d067b43f45d2
Philip Shishov <philshishov@gmail.com>
2020-05-24 15:29:41 +0200

    Refactor js uploadController

--- src/DataGate/Web/DataGate.Web/wwwroot/js/controllers/uploadController.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/controllers/uploadController.js
@@ -1,29 +1,56 @@
-﻿function uploadModalsGet(token, json) {
-    const placeholderDocument = $('#modal-placeholder-document');
-    const placeholderAgreement = $('#modal-placeholder-agreement');
+﻿function uploadModals(token, json) {
+    const btnDocTextContent = document.getElementById('btn-upload-document').textContent.trim();
+    let btnTextContent;
 
-    $(document).on('click', '#btn-upload-document', function (event) {
+    if (btnDocTextContent) {
+        btnTextContent = btnDocTextContent;
+    } else {
+        btnTextContent = btnAgrTextContent;
+    }
+
+    let placeholderElement;
+    let url;
+    if (btnTextContent === 'Upload Agreement') {
+        placeholderElement = $('#modal-placeholder-agreement');
+        url = '/loadAgrUpload';
+    } else {
+        placeholderElement = $('#modal-placeholder-document');
+        url = '/loadDocUpload';
+    }
+
+    $(document).on('click', 'button[data-toggle="ajax-modal"]', function (event) {
         $.get({
-            url: '/loadDocUpload',
+            url: url,
             data: json,
             contentType: 'application/json; charset=utf-8',
             headers: { 'X-CSRF-TOKEN': token },
         }).done(function (data) {
-            placeholderDocument.html(data);
-            placeholderDocument.find('.modal').modal('show');
+            placeholderElement.html(data);
+            placeholderElement.find('.modal').modal('show');
         });
     });
 
-    $(document).on('click', '#btn-upload-agreement', function (event) {
-        $.get({
-            url: '/loadAgrUpload',
-            data: json,
-            contentType: 'application/json; charset=utf-8',
+    placeholderElement.on('click', '[data-save="modal"]', function (event) {
+        event.preventDefault();
+
+        const form = $(this).parents('.modal').find('form');
+        const token = $('input[name=__RequestVerificationToken]', form).val();
+        debugger;
+        const actionUrl = form.attr('action');
+        const dataToSend = new FormData(form.get(0));
+
+        $.ajax({
+            url: actionUrl,
+            method: 'POST',
+            data: dataToSend,
             headers: { 'X-CSRF-TOKEN': token },
+            processData: false,
+            contentType: false
         }).done(function (data) {
-            placeholderAgreement.html(data);
-            placeholderAgreement.find('.modal').modal('show');
+            const newBody = $('.modal-body', data);
+            placeholderElement.find('.modal-body').replaceWith(newBody);
+        }).fail(function (request, status, error) {
+            alert(request.responseText);
         });
     });
-}
-
+};
