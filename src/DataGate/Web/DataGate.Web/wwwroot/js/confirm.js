951dc99dc40390196c8603d21c5777e2b4446ad3
Philip Shishov <philshishov@gmail.com>
2020-05-26 22:22:33 +0200

    Enclose js functions into global ones

---
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/confirm.js
@@ -0,0 +1,50 @@
+﻿// Confirm create
+(function () {
+    const btn = document.getElementById('confirmCreateBtn');
+
+    if (btn) {
+        btn.addEventListener('click',
+            function (event) {
+                let con = confirm('Are you sure you want to create this?');
+
+                if (!con) {
+                    event.preventDefault();
+                }
+            });
+    }
+})();
+
+
+// Confirm delete
+(function () {
+    const buttons = document.getElementsByClassName('confirmDelBtn');
+
+    if (buttons) {
+        for (var btn of buttons) {
+            btn.addEventListener('click',
+                function (event) {
+                    let con = confirm('Are you sure you want to delete this?');
+
+                    if (!con) {
+                        event.preventDefault();
+                    }
+                });
+        }
+    }
+})();
+
+// Confirm edit
+(function () {
+    const btn = document.getElementById('confirmEditBtn');
+
+    if (btn) {
+        btn.addEventListener('click',
+            function (event) {
+                let con = confirm('Are you sure you want to update this?');
+
+                if (!con) {
+                    event.preventDefault();
+                }
+            });
+    }
+})();
