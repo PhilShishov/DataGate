ae2aa95e6870289ac3133448a98723c4b08bc0eb
Philip Shishov <philshishov@gmail.com>
2020-05-28 15:33:46 +0200

    Add design part for global search action

---
+++ src/DataGate/Web/DataGate.Web/Controllers/SearchController.cs
@@ -0,0 +1,14 @@
+﻿namespace DataGate.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class SearchController : Controller
+    {
+        public IActionResult Search(string searchTerm)
+        {
+            return this.View();
+        }
+    }
+}
