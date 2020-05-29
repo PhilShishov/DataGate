23e15b99fe8001c54fff52708011bcf4021b32bb
Philip Shishov <philshishov@gmail.com>
2020-05-29 18:12:18 +0200

    Wrote js function to maintain only one opened menu at a time - close the others if opened

--- src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
@@ -11,16 +11,75 @@
 (function () {
     const sidenavIcons = document.getElementsByClassName(HTML.SIDENAV_ICONS);
     const sideNavFooter = document.getElementById(HTML.SIDENAV_FOOTER);
+    const sideNavDropdowns = document.getElementsByClassName(HTML.SIDENAV_BTNS_DROPDOWN);
+    const searchMenuToogler = document.getElementsByClassName('open-search-box')[0];
+    const searchMenuParent = document.getElementsByClassName('search-form-wrapper-responsive')[0];
+    const searchMenu = document.getElementsByClassName('search-form-wrapper')[0];
+    const userMenuToogler = document.getElementsByClassName('responsive-menu')[0];
+    const sidenav = document.getElementById(HTML.SIDENAV_MAIN);
 
+    // Toogle sidenav menu and sidenavdropdowns
     if (sidenavIcons) {
         for (let icon of sidenavIcons) {
             icon.addEventListener('click', toggleSideNavMenu);
         }
+
+        for (let i = 0; i < sideNavDropdowns.length; i++) {
+            sideNavDropdowns[i].addEventListener('click', function () {
+                this.classList.toggle('active');
+                let dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === 'block') {
+                    dropdownContent.style.display = 'none';
+                } else {
+                    dropdownContent.style.display = 'block';
+                }
+            });
+        }
+    }
+
+    // Toogle search menu
+    if (searchMenuToogler) {
+        searchMenuToogler.addEventListener('click', toggleSearchMenu);
+
+        function toggleSearchMenu() {
+
+            if (userMenuToogler.classList.contains('open')) {
+                userMenuToogler.classList.toggle('open');
+            }
+            if (sidenav.style.width != '0px') {
+                sidenav.style.width = '0px';
+                sideNavFooter.style.display = 'none';
+            }
+
+            searchMenuParent.classList.toggle('opened');
+            const searchMenuStatus = searchMenu.classList.contains('d-none');
+
+            if (searchMenuStatus) {
+                searchMenu.classList.remove('d-none');
+            } else {
+                searchMenu.classList.add('d-none');
+            }
+        };
+    }
+
+    // Toggle user menu
+    if (userMenuToogler) {
+        userMenuToogler.addEventListener('click', () => {
+
+            if (!searchMenu.classList.contains('d-none')) {
+                searchMenuParent.classList.toggle('opened');
+                searchMenu.classList.add('d-none');
+            }
+            if (sidenav.style.width != '0px') {
+                sidenav.style.width = '0px';
+                sideNavFooter.style.display = 'none';
+            }
+            userMenuToogler.classList.toggle('open');
+        })
     }
 
     function toggleSideNavMenu() {
         const sidenavBtnsClose = document.getElementsByClassName(HTML.SIDENAV_BTN_CLOSE);
-        const sidenav = document.getElementById(HTML.SIDENAV_MAIN);
 
         for (var btn of sidenavBtnsClose) {
             btn.addEventListener('click', closeNav);
@@ -29,6 +88,13 @@
         const navMenuStatus = sidenav.style.width;
 
         if (navMenuStatus == '0px' || navMenuStatus === '') {
+            if (!searchMenu.classList.contains('d-none')) {
+                searchMenuParent.classList.toggle('opened');
+                searchMenu.classList.add('d-none');
+            }
+            if (userMenuToogler.classList.contains('open')) {
+                userMenuToogler.classList.toggle('open');
+            }
             sidenav.style.width = '250px';
             sideNavFooter.style.display = 'block';
         } else {
@@ -40,46 +106,5 @@
             sidenav.style.width = '0px';
             sideNavFooter.style.display = 'none';
         }
-    }
-
-})();
-
-(function () {
-    const sideNavDropdowns = document.getElementsByClassName(HTML.SIDENAV_BTNS_DROPDOWN);
-
-    for (let i = 0; i < sideNavDropdowns.length; i++) {
-        sideNavDropdowns[i].addEventListener('click', function () {
-            this.classList.toggle('active');
-            let dropdownContent = this.nextElementSibling;
-            if (dropdownContent.style.display === 'block') {
-                dropdownContent.style.display = 'none';
-            } else {
-                dropdownContent.style.display = 'block';
-            }
-        });
-    }
-})();
-
-$(HTML.NAVBAR_USER_MENU).on('click', function () {
-    $(this).toggleClass('open');
-});
-
-(function () {
-    const searchMenuToogler = document.getElementsByClassName('search-form-wrapper-responsive')[0];
-    const searchMenu = document.getElementsByClassName('search-form-wrapper')[0];
-
-    if (searchMenuToogler) {
-        searchMenuToogler.addEventListener('click', toggleSearchMenu);
-
-        function toggleSearchMenu() {
-            searchMenuToogler.classList.toggle('opened');
-            const searchMenuStatus = searchMenu.classList.contains('d-none');
-
-            if (searchMenuStatus) {
-                searchMenu.classList.remove('d-none');
-            } else {
-                searchMenu.classList.add('d-none');
-            }
-        };
     }
 })();
