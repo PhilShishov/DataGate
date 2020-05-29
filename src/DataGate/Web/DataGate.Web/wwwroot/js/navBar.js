8be43e240847b06d0d3402795c33764c32e7b9bd
Philip Shishov <philshishov@gmail.com>
2020-05-29 09:38:03 +0200

    Complete responsive user menu

--- src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
@@ -4,6 +4,7 @@
     SIDENAV_FOOTER: 'sidenav-footer',
     SIDENAV_BTN_CLOSE: 'sidenav-btn-close',
     SIDENAV_BTNS_DROPDOWN: 'sidenav-dropdown-btn',
+    NAVBAR_USER_MENU: '.responsive-menu',
 };
 
 (function () {
@@ -17,7 +18,7 @@
     function toggleSideNavMenu() {
         const sidenavBtnClose = document.getElementById(HTML.SIDENAV_BTN_CLOSE);
         sidenavBtnClose.addEventListener('click', closeNav);
-        navMenuStatus = document.getElementById(HTML.SIDENAV).style.width;
+        const navMenuStatus = document.getElementById(HTML.SIDENAV).style.width;
         if (navMenuStatus == '0px' || navMenuStatus === '') {
             document.getElementById(HTML.SIDENAV).style.width = '250px';
             sideNavFooter.style.display = 'block';
@@ -49,6 +50,25 @@
     }
 })();
 
-$('.responsive-menu').on('click', function () {
+$(HTML.NAVBAR_USER_MENU).on('click', function () {
     $(this).toggleClass('open');
 });
+
+(function () {
+    const navbarResponsiveMenuToogler = document.getElementsByClassName('open-responsive-menu')[0];
+    const userMenu = document.getElementsByClassName('responsive-menu-user')[0];
+
+    //if (navbarResponsiveMenuToogler) {
+    //    navbarResponsiveMenuToogler.addEventListener('click', toggleUserMenu);
+
+    //    function toggleUserMenu() {
+    //        const userMenuStatus = userMenu.classList.contains('d-none');
+
+    //        if (userMenuStatus) {
+    //            userMenu.classList.remove('d-none');
+    //        } else {
+    //            userMenu.classList.add('d-none');
+    //        }
+    //    };
+    //}
+})();
