d607ba00243dd9060fd27422300d6f9b27c89d5e
Philip Shishov <philshishov@gmail.com>
2020-05-30 13:12:06 +0200

    Update gitignore

---
+++ src/DataGate/Web/DataGate.Web/Configuration/DataProtectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace DataGate.Web.Configuration
+{
+    using System.IO;
+
+    using Microsoft.AspNetCore.DataProtection;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DataProtectionConfiguration
+    {
+        public static IServiceCollection ConfigureDataProtection(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Working path @"D:\HostingSpaces\FM86\pharusdatagate.com\data\keys"
+            services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo(configuration.GetValue<string>("WebWiz:KeysPath")));
+            return services;
+        }
+    }
+}
