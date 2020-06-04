76610cdad3d4b6194c6f99f6a8348f06f8d117c5
Philip Shishov <philshishov@gmail.com>
2020-06-04 11:40:59 +0200

    Optimize global search and search view

--- src/DataGate/Web/DataGate.Web.ViewModels/Search/SearchListAllViewModel.cs
+++ src/DataGate/Web/DataGate.Web.ViewModels/Search/SearchListAllViewModel.cs
@@ -11,36 +11,8 @@
 
         public IEnumerable<ResultViewModel> Results { get; set; }
 
-        public int PagesCount { get; set; }
+        public string Date { get; set; }
 
-        public int CurrentPage { get; set; }
-
-        public string SearchString { get; set; }
-
-        public int NextPage
-        {
-            get
-            {
-                if (this.CurrentPage >= this.PagesCount)
-                {
-                    return 1;
-                }
-
-                return this.CurrentPage + 1;
-            }
-        }
-
-        public int PreviousPage
-        {
-            get
-            {
-                if (this.CurrentPage <= 1)
-                {
-                    return this.PagesCount;
-                }
-
-                return this.CurrentPage - 1;
-            }
-        }
+        public string SearchTerm { get; set; }
     }
 }
