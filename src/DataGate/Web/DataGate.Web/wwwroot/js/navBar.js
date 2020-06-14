5d3e89f4ee4586de1582b59a08b519988f20a809
Philip Shishov <philshishov@gmail.com>
2020-06-14 17:18:13 +0200

    Image insert into css for faster loading

--- src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/navBar.js
@@ -1,10 +1,4 @@
 ﻿const HTML_NAVBAR = {
-    SIDENAV_MAIN: 'main-sidenav',
-    SIDENAV_RESPONSIVE: 'sidenav-responsive',
-    SIDENAV_ICONS: 'sidenav-icon',
-    SIDENAV_FOOTER: 'sidenav-footer',
-    SIDENAV_BTN_CLOSE: 'sidenav-btn-close',
-    SIDENAV_BTNS_DROPDOWN: 'sidenav-dropdown-btn',
     USER_MENU_TOOGLER: 'responsive-menu',
     SEARCH_MENU_PARENT: 'search-form-wrapper-responsive',
     SEARCH_MENU: 'search-form-wrapper',
@@ -18,33 +12,10 @@ const CLASSES_NAVBAR = {
 };
 
 (function () {
-    const sidenav = document.getElementById(HTML_NAVBAR.SIDENAV_MAIN);
-    const sidenavIcons = document.getElementsByClassName(HTML_NAVBAR.SIDENAV_ICONS);
-    const sideNavFooter = document.getElementById(HTML_NAVBAR.SIDENAV_FOOTER);
-    const sideNavDropdowns = document.getElementsByClassName(HTML_NAVBAR.SIDENAV_BTNS_DROPDOWN);
     const searchMenuParent = document.getElementsByClassName(HTML_NAVBAR.SEARCH_MENU_PARENT)[0];
     const searchMenu = document.getElementsByClassName(HTML_NAVBAR.SEARCH_MENU)[0];
     const searchMenuToogler = document.getElementsByClassName(HTML_NAVBAR.SEARCH_MENU_TOOGLER)[0];
     const userMenuToogler = document.getElementsByClassName(HTML_NAVBAR.USER_MENU_TOOGLER)[0];
-
-    // Toogle sidenav menu and sidenavdropdowns
-    if (sidenavIcons) {
-        for (let icon of sidenavIcons) {
-            icon.addEventListener('click', toggleSideNavMenu);
-        }
-
-        for (let i = 0; i < sideNavDropdowns.length; i++) {
-            sideNavDropdowns[i].addEventListener('click', function () {
-                this.classList.toggle(CLASSES_NAVBAR.ACTIVE);
-                let dropdownContent = this.nextElementSibling;
-                if (dropdownContent.style.display === 'block') {
-                    dropdownContent.style.display = 'none';
-                } else {
-                    dropdownContent.style.display = 'block';
-                }
-            });
-        }
-    }
 
     // Toogle search menu
     if (searchMenuToogler) {
@@ -55,10 +26,6 @@ const CLASSES_NAVBAR = {
             if (userMenuToogler.classList.contains(CLASSES_NAVBAR.OPEN)) {
                 userMenuToogler.classList.toggle(CLASSES_NAVBAR.OPEN);
             }
-            //if (sidenav.style.width != '0px') {
-            //    sidenav.style.width = '0px';
-            //    sideNavFooter.style.display = 'none';
-            //}
 
             searchMenuParent.classList.toggle(CLASSES_NAVBAR.OPENED);
             const searchMenuStatus = searchMenu.classList.contains('d-none');
@@ -78,41 +45,7 @@ const CLASSES_NAVBAR = {
                 searchMenuParent.classList.toggle(CLASSES_NAVBAR.OPENED);
                 searchMenu.classList.add('d-none');
             }
-            //if (sidenav.style.width != '0px') {
-            //    sidenav.style.width = '0px';
-            //    sideNavFooter.style.display = 'none';
-            //}
             userMenuToogler.classList.toggle(CLASSES_NAVBAR.OPEN);
         })
-    }
-
-    function toggleSideNavMenu() {
-        const sidenavBtnsClose = document.getElementsByClassName(HTML_NAVBAR.SIDENAV_BTN_CLOSE);
-
-        for (var btn of sidenavBtnsClose) {
-            btn.addEventListener('click', closeNav);
-        }
-
-        const navMenuStatus = sidenav.style.width;
-
-        if (navMenuStatus == '0px' || navMenuStatus === '') {
-            if (!searchMenu.classList.contains('d-none')) {
-                searchMenuParent.classList.toggle(CLASSES_NAVBAR.OPENED);
-                searchMenu.classList.add('d-none');
-            }
-            if (userMenuToogler.classList.contains(CLASSES_NAVBAR.OPEN)) {
-                userMenuToogler.classList.toggle(CLASSES_NAVBAR.OPEN);
-            }
-            sidenav.style.width = '250px';
-            sideNavFooter.style.display = 'block';
-        } else {
-            sidenav.style.width = '0px';
-            sideNavFooter.style.display = 'none';
-        }
-
-        function closeNav() {
-            sidenav.style.width = '0px';
-            sideNavFooter.style.display = 'none';
-        }
     }
 })();
