a4794b4279c5da9d77dbadfb602daa25e5d219e9
Philip Shishov <philshishov@gmail.com>
2020-06-19 15:48:18 +0200

    use of Chart js refactor

--- src/DataGate/Web/DataGate.Web/wwwroot/js/controllers/detailsController.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/controllers/detailsController.js
@@ -1,4 +1,14 @@
-﻿const HTML_MENU = {
+﻿window.chartColors = [
+    'rgb(54, 162, 235)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(153, 102, 255)',
+    'rgb(255, 99, 132)',
+    'rgb(201, 203, 207)'
+];
+
+const HTML_MENU = {
     BTN_SUBENTITIES: '#btn-open-subentities',
     BTN_DISTINCT: '#btn-open-distinct',
     BTN_CHART: '#btn-open-chart',
