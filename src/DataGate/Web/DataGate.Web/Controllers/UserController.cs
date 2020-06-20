5b591e715f89fb80d3ade88235cfe19d9f1442a7
Philip Shishov <philshishov@gmail.com>
2020-06-20 07:24:41 +0200

    Add slick js carousel for newest shareclasses

--- src/DataGate/Web/DataGate.Web/Controllers/UserController.cs
+++ src/DataGate/Web/DataGate.Web/Controllers/UserController.cs
@@ -1,15 +1,31 @@
 ﻿namespace DataGate.Web.Controllers
 {
+    using System.Linq;
+
+    using DataGate.Data.Common.Repositories;
+    using DataGate.Data.Models.Entities;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [Authorize]
     public class UserController : BaseController
     {
+        private readonly IRepository<TbHistoryShareClass> repository;
+
+        public UserController(IRepository<TbHistoryShareClass> repository)
+        {
+            this.repository = repository;
+        }
+
         [Route("userpanel")]
         public IActionResult Index()
         {
-            return this.View();
+            var latest = this.repository.All()
+                .OrderByDescending(sc => sc.ScInitialDate)
+                .Take(10)
+                .ToList();
+
+            return this.View(latest);
         }
     }
 }
