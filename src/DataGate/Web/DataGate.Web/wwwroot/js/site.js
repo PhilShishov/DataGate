a14c799a34d2dda5f2f3a993aa66095952e2e83f
Philip Shishov <philshishov@gmail.com>
2020-07-29 18:38:39 +0200

    Update no link for language button

--- src/DataGate/Web/DataGate.Web/wwwroot/js/site.js
+++ src/DataGate/Web/DataGate.Web/wwwroot/js/site.js
@@ -15,3 +15,13 @@ function backToTop() {
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
+
+$(document).ready(function () {
+    $('a[href].no-link').each(function () {
+        let href = this.href;
+
+        $(this).removeAttr('href').css('cursor', 'pointer').click(function () {
+            window.open(href, '_self');
+        });
+    });
+});
