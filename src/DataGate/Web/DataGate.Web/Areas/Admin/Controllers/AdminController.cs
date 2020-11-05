77fdf5fc0098e48489babe2363c84defa03f9ca3
Philip Shishov <philshishov@gmail.com>
2020-11-05 23:24:49 +0100

    Setup datatable filter column on all agreements view

--- src/DataGate/Web/DataGate.Web/Areas/Admin/Controllers/AdminController.cs
+++ src/DataGate/Web/DataGate.Web/Areas/Admin/Controllers/AdminController.cs
@@ -16,7 +16,6 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Localization;
     using Microsoft.Extensions.Logging;
 
     [Area("Admin")]
