45669b034f2c2f5805b9853dacff9084f06ab4cc
Philip Shishov <philshishov@gmail.com>
2020-12-15 21:01:23 +0100

    Prepare CI/CD

--- src/DataGate/Tests/DataGate.Web.Tests/Controllers/Files/UploadControllerTests.cs
+++ src/DataGate/Tests/DataGate.Web.Tests/Controllers/Files/UploadControllerTests.cs
@@ -1,32 +1,32 @@
-﻿namespace DataGate.Web.Tests.Controllers.Files
-{
-    using MyTested.AspNetCore.Mvc;
-    using Xunit;
+﻿//namespace DataGate.Web.Tests.Controllers.Files
+//{
+//    using MyTested.AspNetCore.Mvc;
+//    using Xunit;
 
-    using DataGate.Web.Controllers.Files;
-    using DataGate.Web.InputModels.Files;
+//    using DataGate.Web.Controllers.Files;
+//    using DataGate.Web.InputModels.Files;
 
-    public class UploadControllerTests
-    {
-        [Theory]
-        [InlineData()]
-        [InlineData()]
-        [InlineData()]
-        [InlineData()]
-        public void OnPostUploadDocumentAsync_WithInvalidModelState_ShouldReturnPartialView()
-        {
-            var model = new UploadDocumentInputModel()
-            {
-                DocumentType = "DocumentType",
-                //FileToUpload = 
-            };
+//    public class UploadControllerTests
+//    {
+//        [Theory]
+//        [InlineData()]
+//        [InlineData()]
+//        [InlineData()]
+//        [InlineData()]
+//        public void OnPostUploadDocumentAsync_WithInvalidModelState_ShouldReturnPartialView()
+//        {
+//            var model = new UploadDocumentInputModel()
+//            {
+//                DocumentType = "DocumentType",
+//                //FileToUpload = 
+//            };
 
-            MyController<UploadController>
-                .Instance()
-                //.WithData(data => data.WithEntities<ApplicationDbContext>(shareClass))
-                .Calling(c => c.OnPostUploadDocumentAsync(model))
-                .ShouldReturn()
-                .PartialView("Upload/_UploadDocument");
-        }
-    }
-}
+//            MyController<UploadController>
+//                .Instance()
+//                //.WithData(data => data.WithEntities<ApplicationDbContext>(shareClass))
+//                .Calling(c => c.OnPostUploadDocumentAsync(model))
+//                .ShouldReturn()
+//                .PartialView("Upload/_UploadDocument");
+//        }
+//    }
+//}
