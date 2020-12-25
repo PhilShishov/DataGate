b70c8b43e9d7a5a9f447f827b4750925df2f4379
Philip Shishov <philshishov@gmail.com>
2020-12-25 16:06:53 +0100

    Add Notification ef core db models

---
+++ src/DataGate/Data/DataGate.Data.Models/Enums/NotificationStatus.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) DataGate Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DataGate.Data.Models.Users.Enums
+{
+    public enum NotificationStatus
+    {
+        Read = 1,
+        Unread = 2,
+    }
+}
---
+++ src/DataGate/Data/DataGate.Data.Models/Users/UserNotification.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) DataGate Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DataGate.Data.Models.Users
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using DataGate.Data.Models.Users.Enums;
+
+    public class UserNotification
+    {
+        public UserNotification()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        public NotificationStatus Status { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public string LinkUrl { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public bool IsOpened { get; set; }
+    }
+}
