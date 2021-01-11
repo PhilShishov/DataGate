0c9681817d79c1d895365b5f316109a9727643fc
Andrii Tkach <andrii.tkach@vilmate.com>
2021-01-11 13:52:04 +0200

    Implemented ViewSetup tests

---
+++ src/DataGate/Tests/DataGate.Services.Data.Tests/TestData/EntityDetailsServiceTestData.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) DataGate Project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DataGate.Services.Data.Entities;
+
+namespace DataGate.Services.Data.Tests.TestData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using DataGate.Common;
+    using DataGate.Data;
+    using DataGate.Services.SqlClient;
+    using DataGate.Web.Helpers;
+    using DataGate.Web.ViewModels.Queries;
+
+    using Microsoft.Extensions.Configuration;
+
+    public static class EntityDetailsServiceTestData
+    {
+
+        public static EntityDetailsService CreateService(ApplicationDbContext context, IConfiguration configuration)
+        {
+            var sqlManager = new SqlQueryManager(configuration);
+
+            var service = new EntityDetailsService(sqlManager);
+            return service;
+        }
+    }
+}
